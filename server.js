ca331d1c81a591f2bf86fdac8727ea42e05ef9fe
ProgXx <ProgXx@users.noreply.github.com>
2015-06-10 14:07:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Require dependencis to be used in project.
 var express = require("express");
 var path = require('path');
 var bodyParser = require("body-parser");
@@ -5,12 +6,14 @@ var morgan = require("morgan");
 var config = require('./config');
 var mongoose = require("mongoose");
 
+
 var app = express();
 
 var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
 
+//Connect to the mongoose database.
 mongoose.connect(config.database,function(err){
 	if(err){
 		console.log(err);
@@ -19,21 +22,22 @@ mongoose.connect(config.database,function(err){
 	}
 });
 
+//Define the middleware 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Create a route for http://localhost:3000/api
 var api = require('./app/routes/api')(app,express,io);
 app.use('/api',api); // /api prefix for all the api
 
-
+//Any other route will be redirected to the index.html
 app.get('*',function(req,res){
 	res.sendFile(__dirname + '/public/app/views/index.html');
 })
 
-
+//Start the server.
 http.listen(config.port,function(err){
 	if(err){
 		console.log(err);
